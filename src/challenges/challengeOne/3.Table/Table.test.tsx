3fb362250bc8f5676f4bbf22a340424b4431193a
Umesh <umesh@codewalnut.com>
2023-06-09 14:21:37 +0530

    Add:  test case

--- src/challenges/challengeOne/3.Table/Table.test.tsx
+++ src/challenges/challengeOne/3.Table/Table.test.tsx
@@ -1,8 +1,33 @@
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-// import { Table } from "./Table";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Table } from "./Table";
 
-// describe("Table", () => {
-//   it("renders...", () => {});
-// });
+describe("Table", () => {
+  const columns = [
+    { property: "name", label: "Name" },
+    { property: "email", label: "Email" },
+  ];
+
+  const data = [
+    { id: 1, name: "John Doe", email: "johndoe@example.com" },
+    { id: 2, name: "Jane Smith", email: "janesmith@example.com" },
+  ];
+
+  it("renders table with correct columns and data", () => {
+    render(<Table columns={columns} data={data} />);
+
+    // Check table header
+    columns.forEach((column) => {
+      const columnHeader = screen.getByText(column.label);
+      expect(columnHeader).toBeInTheDocument();
+    });
+
+    // Check table rows and cells
+    data.forEach((item) => {
+      columns.forEach((column) => {
+        const cell = screen.getByText(item[column.property]);
+        expect(cell).toBeInTheDocument();
+      });
+    });
+  });
+});
--- src/challenges/challengeOne/4.Form/Form.test.tsx
+++ src/challenges/challengeOne/4.Form/Form.test.tsx
@@ -1,8 +1,77 @@
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-// import { Form } from "./Form";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Form } from "./Form";
 
-// describe("Form", () => {
-//   it("renders...", () => {});
-// });
+describe("Form", () => {
+  const fields = [
+    {
+      label: "Name",
+      type: "text",
+      name: "name",
+      placeholder: "Enter your name",
+      value: "",
+      id: "name-input",
+    },
+    {
+      label: "Email",
+      type: "email",
+      name: "email",
+      placeholder: "Enter your email",
+      value: "",
+      id: "email-input",
+    },
+  ];
+
+  it("renders label for each form field", () => {
+    const fields: FormProps["fields"] = [
+      {
+        label: "Field 1",
+        type: "text",
+        name: "field1",
+        placeholder: "Enter Field 1",
+        value: "",
+        id: "field1",
+      },
+      {
+        label: "Field 2",
+        type: "text",
+        name: "field2",
+        placeholder: "Enter Field 2",
+        value: "",
+        id: "field2",
+      },
+    ];
+
+    render(<Form fields={fields} onSubmit={() => {}} />);
+
+    fields.forEach((field) => {
+      const labelElement = screen.getByLabelText(field.label);
+      expect(labelElement).toBeInTheDocument();
+    });
+  });
+
+  it("submits form with entered values", async () => {
+    const onSubmit = jest.fn();
+
+    render(<Form fields={fields} onSubmit={onSubmit} />);
+
+    const nameInput = screen.getByPlaceholderText("Enter your name");
+    const emailInput = screen.getByPlaceholderText("Enter your email");
+    const submitButton = screen.getByRole("button", { name: "Submit" });
+
+    await userEvent.type(nameInput, "John Doe");
+    await userEvent.type(emailInput, "johndoe@example.com");
+
+    expect(nameInput).toHaveValue("John Doe");
+    expect(emailInput).toHaveValue("johndoe@example.com");
+
+    await userEvent.click(submitButton);
+
+    expect(onSubmit).toHaveBeenCalledTimes(1);
+    expect(onSubmit).toHaveBeenCalledWith({
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
+  });
+});
